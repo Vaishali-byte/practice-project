35411ad1e83ccc4c69b9b2ba4345e6cba65140e8
Vaishali Pawar <vpwork9@gmail.com>
2022-10-25 19:20:55 +0100

    needs to fix it in update department in ROUTE

--- application/forms.py
+++ application/forms.py
@@ -8,9 +8,15 @@ from wtforms import IntegerField, StringField, SubmitField, IntegerField
 class EmployeeForm(FlaskForm):
     employee_id = IntegerField("employee_id")
     name = StringField("Name")
+    fk_did = IntegerField("Department_id")
     age =IntegerField("age")
     position = StringField("position")
     submit = SubmitField("Submit")
+    
+class DepartmentForm(FlaskForm):
+    name = StringField("Name")
+    submit = SubmitField("Submit")
+
 
 
     
--- application/models.py
+++ application/models.py
@@ -1,6 +1,10 @@
+# How your database are going to look
 from flask_sqlalchemy import SQLAlchemy
 from application import db
  
+class Departments(db.Model):
+    did = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
 
 class EmployeeModel(db.Model):   
     employee_id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +12,7 @@ class EmployeeModel(db.Model):
     name = db.Column(db.String())
     age = db.Column(db.Integer())
     position = db.Column(db.String(80))
+    fk_did = db.Column(db.Integer, db.ForeignKey('departments.did'))
 
     
 
