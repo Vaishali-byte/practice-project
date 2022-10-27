ec8ea7b2fe0f520277ed2feb4a4d9ba657c0d28c
Ubuntu <azureuser@employeevm.34ekr1kh201ezal3xtfe2oqasa.dx.internal.cloudapp.net>
2022-10-27 11:23:59 +0000

    jenkins file added

--- db/create.sql
+++ db/create.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS departments (
 );
 
 CREATE TABLE IF NOT EXISTS EmployeeModel ( 
-		employee_id INT PRIMARY KEY AUTO_INCREMENT,
-		name VARCHAR(20)
-		age INT (11)
-		position VARCHAR(30)
-		fk_did INT,
-		FOREIGN KEY (fk_did) REFERENCES departments(did)
+	employee_id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(20)
+	age INT (11)
+	position VARCHAR(30)
+	fk_did INT,
+	FOREIGN KEY (fk_did) REFERENCES departments(did)
